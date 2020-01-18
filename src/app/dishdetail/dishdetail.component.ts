0169cc4779e6604d6adf7ff2488df109994d02ff
Sandeep Sodhi <vickysodhi15@gmail.com>
2020-01-18 16:40:13 -0500

    consolidating animation changes to seperate file

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -8,25 +8,14 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { switchMap } from 'rxjs/operators';
 import { Comment } from '../shared/comment';
-import { MatGridTileHeaderCssMatStyler } from '@angular/material';
-import { trigger, state, style, animate, transition } from '@angular/animations';
+import { visibility } from '../animations/app.animation';
 
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
   styleUrls: ['./dishdetail.component.scss'],
   animations: [
-    trigger('visibility', [
-      state('shown', style({
-        transform: 'scale(1.0)',
-        opacity: 1
-      })),
-      state('hidden', style({
-        transform: 'scale(0.5)',
-        opacity: 0
-      })),
-      transition('* => *', animate('0.5s ease-in-out'))
-    ])
+    visibility()
   ]
 })
 export class DishdetailComponent implements OnInit {
